d09e8b5b1ffe1654ad98c845c8d8882532202c90
zayant21 <zayantofeeq@csu.fullerton.edu>
2021-10-12 15:27:42 -0700

    Added Requirements file

--- Subscrap/views.py
+++ Subscrap/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse   
 
 def home(request):
-    return HttpResponse('<h1> Subscrap Home</h1>')
+    return HttpResponse('<h1> Home</h1>')
+    #return render(request, 'subscrap/home.html')
 
 def login(request):
     return HttpResponse('<h1> Login</h1>')
--- subscription_app_project/urls.py
+++ subscription_app_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Subscrap/', include('Subscrap.urls')),
+    path('', include('Subscrap.urls')),
 ]
